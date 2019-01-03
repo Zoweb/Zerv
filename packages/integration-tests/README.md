da1b37650f633ec35344f5857025570c5ef45c2f
Zach Barham <zachymach@live.com>
2019-01-03 18:08:48 +1000

    add integration tests package

---
+++ packages/integration-tests/README.md
@@ -0,0 +1,11 @@
+# `@zerv/integration-tests`
+
+> TODO: description
+
+## Usage
+
+```
+const integrationTests = require('@zerv/integration-tests');
+
+// TODO: DEMONSTRATE API
+```
--- packages/integration-tests/lib/app.ts
+++ packages/integration-tests/lib/app.ts
@@ -1,7 +1,22 @@
 import Zerv from "@zerv/framework";
+//Zerv.disableDebugLogging();
 
-console.log("Hello, world!");
+const app = new Zerv("#app");
 
-const app = new Zerv({
-    app: "#app"
-});
+app.data.title = "Test App";
+app.data.styleAttribute = "background: blue";
+
+app.data.user = {
+    name: "test"
+};
+app.data.user.email = {
+    name: "testing",
+    host: "example.com"
+};
+app.data.user.email.other = {
+    port: 80
+};
+
+app.render();
+
+setTimeout(() => app.data.styleAttribute = "font-weight:400", 1000);
